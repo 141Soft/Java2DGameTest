b29e72cca04fc0741af96e8e84a020e287c8c79c
141Soft <161060240+141Soft@users.noreply.github.com>
2024-12-27 18:16:33 +0000

    Updates key pairs for movement

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -21,13 +21,25 @@ public class KeyHandler extends KeyAdapter{
 			case KeyEvent.VK_W:
 				upPressed = true;
 				break;
+			case KeyEvent.VK_UP:
+				upPressed = true;
+				break;
 			case KeyEvent.VK_S:
+				downPressed = true;
+				break;
+			case KeyEvent.VK_DOWN:
 				downPressed = true;
 				break;
 			case KeyEvent.VK_A:
 				leftPressed = true;
 				break;
+			case KeyEvent.VK_LEFT:
+				leftPressed = true;
+				break;
 			case KeyEvent.VK_D:
+				rightPressed = true;
+				break;
+			case KeyEvent.VK_RIGHT:
 				rightPressed = true;
 				break;
 		}
@@ -42,13 +54,25 @@ public class KeyHandler extends KeyAdapter{
 			case KeyEvent.VK_W:
 				upPressed = false;
 				break;
+			case KeyEvent.VK_UP:
+				upPressed = false;
+				break;
 			case KeyEvent.VK_S:
+				downPressed = false;
+				break;
+			case KeyEvent.VK_DOWN:
 				downPressed = false;
 				break;
 			case KeyEvent.VK_A:
 				leftPressed = false;
 				break;
+			case KeyEvent.VK_LEFT:
+				leftPressed = false;
+				break;
 			case KeyEvent.VK_D:
+				rightPressed = false;
+				break;
+			case KeyEvent.VK_RIGHT:
 				rightPressed = false;
 				break;
 		}
