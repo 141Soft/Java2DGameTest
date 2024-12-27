05c525821ec3af248bdea4e4c92c32d4ddfeb12e
141Soft <161060240+141Soft@users.noreply.github.com>
2024-12-27 17:04:53 +0000

    Implements full walk cycle and stationary pose for player entity

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -9,4 +9,7 @@ public class Entity {
 	
 	public BufferedImage up, up1, up2, down, down1, down2, left, left1, left2, right, right1, right2;
 	public String direction;
+	
+	public int spriteCounter = 0;
+	public boolean spriteSwitch = true;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -32,20 +32,32 @@ public class Player extends Entity {
 	public void getPlayerImage() {
 		try {
 			down = ImageIO.read(getClass().getResourceAsStream("/player/down.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("/player/down1.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("/player/down2.png"));
 			up = ImageIO.read(getClass().getResourceAsStream("/player/up.png"));
+			up1 = ImageIO.read(getClass().getResourceAsStream("/player/up1.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("/player/up2.png"));
 			left = ImageIO.read(getClass().getResourceAsStream("/player/left.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("/player/left1.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/player/left2.png"));
 			right = ImageIO.read(getClass().getResourceAsStream("/player/right.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("/player/right1.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/player/right2.png"));
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void update() {
-		//Movement
-		move();
-	}
-	
 	public void move() {
+		
+		if(keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
+			spriteCounter++;
+			if(spriteCounter >= 20) {
+				spriteSwitch = !spriteSwitch;
+				spriteCounter = 0;
+			}
+		}
+		
 		if(keyH.upPressed == true) {
 			direction="up";
 			y -= speed;
@@ -62,24 +74,36 @@ public class Player extends Entity {
 			direction="right";
 			x += speed;
 		}
+		
+	}
+	
+	public void update() {
+		move();
 	}
 	
 	public void draw(Graphics2D g2) {
 		BufferedImage image = null;
 		
-		
 		switch(direction) {
 		case "up":
-			image = up;
+			if(!keyH.upPressed) { image = up; }
+			else if(spriteSwitch) { image = up1; }
+			else { image = up2; }
 			break;
 		case "down":
-			image = down;
+			if(!keyH.downPressed) { image = down; }
+			else if(spriteSwitch) { image = down1;}
+			else { image = down2; }
 			break;
 		case "left":
-			image = left;
+			if(!keyH.leftPressed) { image = left; }
+			else if(spriteSwitch) { image = left1; }
+			else { image = left2; }
 			break;
 		case "right":
-			image = right;
+			if(!keyH.rightPressed) { image = right; }
+			else if(spriteSwitch) { image = right1; }
+			else { image = right2; }
 			break;
 		}
 		g2.drawImage(image, x, y, gp.charSpriteWidth, gp.tileSize, null);
